4c3bdb8bbe5d6d0ef31154d0b13248213c11990d
Patrick Rios <patrick.33.rios@hotmail.com>
2022-01-26 03:49:28 -0400

    feat(stylesheet): reponsive adjs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import ProjectProvider from '../contexts/ProjectContext'
+import ListProvider from '../contexts/TaskContext'
+import NotificationProvider from '../contexts/NotificationContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  <ProjectProvider>
+            <NotificationProvider>
+              <ListProvider>
+                <Component {...pageProps} />
+              </ListProvider> 
+            </NotificationProvider>
+          </ProjectProvider>
 }
 
 export default MyApp
